8c2a5ddb191a009533b9aa9fa6938c1081b0dfb8
touhidcodes <touhidcodes@gmail.com>
2023-11-07 02:56:10 +0600

    function generic

---
+++ src/2.5-function-generics.ts
@@ -0,0 +1,45 @@
+{
+  //  Function Generics
+
+  // Normal  Array Function with TS
+  const array = (param: string): string[] => {
+    return [param];
+  };
+
+  //  Function Generics
+  const arrayGeneric = <T>(param: T): T[] => {
+    return [param];
+  };
+
+  const res1 = arrayGeneric<string>("Bangladesh");
+
+  //  Generic Tuple Function
+  const genericTuple = <T, X>(params1: T, params2: X): [T, X] => {
+    return [params1, params2];
+  };
+
+  const res2 = genericTuple<string, number>("Bangladesh", 3);
+
+  //  Example: 
+
+  const addCourseToStudent = <T>(student: T)=>{
+    const course = "Next Level Web Development"
+    return {
+      ...student,
+      course
+    }
+  }
+
+  const student1 = addCourseToStudent({
+    name: "touhid",
+    email: "X@gmail.com"
+    devType: "Next Level Developer"
+  })
+
+  const student2 = addCourseToStudent({
+    name: "urmi",
+    email: "Y@gmail.com"
+   hasWatch: true
+  })
+  //
+}
